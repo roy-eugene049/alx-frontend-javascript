ffcf64e864d3a8d107d86dfe0dca84765b079465
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2023-03-20 16:07:12 +0300

    Intial Commit

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,1 +1,10 @@
-ll
+export default function createIteratorObject(report) {
+  let allEmployees = [];
+  for (const item of Object.values(report.allEmployees)) {
+    allEmployees = [
+      ...allEmployees,
+      ...item,
+    ];
+  }
+  return allEmployees;
+}
