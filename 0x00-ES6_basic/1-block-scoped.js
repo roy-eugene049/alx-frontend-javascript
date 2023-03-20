fa33e1a8a7ea703c235705eecd28dff30b196303
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2023-03-20 15:18:28 +0300

    Initial Commit

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,1 +1,11 @@
-ll
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  }
+
+  return [task, task2];
+}
