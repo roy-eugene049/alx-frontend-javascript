b5d91f57c8795cb16d3af3133b11ca4623d39cce
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2023-03-20 16:04:14 +0300

    For...of Loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,9 @@
-ll
+export default function appendToEachArrayValue(array, appendString) {
+  const arr = [];
+  for (const idx of array) {
+    const value = idx;
+    arr.push(appendString + value);
+  }
+
+  return arr;
+}
