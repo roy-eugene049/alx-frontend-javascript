dc4db761a47629f751b228af22088d4735651456
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2023-03-21 14:34:48 +0300

    Task 2

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,1 +1,14 @@
-ll
+export default function (boolean) {
+  return new Promise((resolve, reject) => {
+    const object = {
+      status: 200,
+      body: 'Success',
+    };
+
+    if (boolean === true) {
+      resolve(object);
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+}
