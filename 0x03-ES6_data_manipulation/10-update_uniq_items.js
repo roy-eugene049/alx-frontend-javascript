9e474906a56e27182e41cc4a7674cdc1a43457f0
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2023-03-28 09:29:08 +0300

    Returns an updated map for all items with initial quantity at 1

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,1 +1,14 @@
-ll
+export default function updateUniqueItems(map) {
+  const list = map;
+
+  if (list instanceof Map) {
+    for (const [key, value] of list) {
+      if (value === 1) {
+        list.set(key, 100);
+      }
+    }
+  } else {
+    throw new Error('Cannot process');
+  }
+  return list;
+}
