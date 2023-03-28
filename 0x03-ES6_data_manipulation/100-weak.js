40418ecc0feb4cbf287457e37d8424b7523ca1ec
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2023-03-28 09:29:48 +0300

    Weak link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,1 +1,11 @@
-ll
+export const weakMap = new WeakMap();
+export function queryAPI(endpoint) {
+  let count = weakMap.get(endpoint) || 0;
+  count += 1;
+
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  } else {
+    weakMap.set(endpoint, count);
+  }
+}
